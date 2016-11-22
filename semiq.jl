060e0065ad6351ca847980cbf81eccb55abef3db
msaitow <msaitow514@gmail.com>
2016-11-22 16:57:09 +0100

    Another change

--- semiq.jl
+++ semiq.jl
@@ -1,26 +1,28 @@
 
-Pkg.add("Gadfly")
+#Pkg.add("Gadfly")
 using Gadfly
-Pkg.add("RDatasets")
+#Pkg.add("RDatasets")
 using RDatasets
-Pkg.add("Cairo")
+#Pkg.add("Cairo")
 using Cairo
 
 semi = readtable("semiquinone.csv")
 
 thistheme = Theme(
     panel_stroke=colorant"black",
-    major_label_font_size=16pt,
+    major_label_font_size=18pt,
     minor_label_font_size=14pt,
-    key_title_font_size=16pt,
-    key_label_font_size=14pt    
+    #key_title_font_size=14pt,
+    key_label_font_size=15pt,
+    key_position = :right,
+    colorkey_swatch_shape = :circle
 )
 
 Gadfly.push_theme(thistheme)
-Gadfly.set_default_plot_size(25cm, 15cm)
+Gadfly.set_default_plot_size(24cm, 14cm)
 
 myLineWidth  = 2.0px
-myPointWidth = 5.0px
+myPointWidth = 6.0px
 
 res = plot(semi,
            
@@ -36,10 +38,13 @@ res = plot(semi,
            layer(x=:x, y=:LPNO_CCSD_total_pairs_,   Geom.point, Geom.line,
                  Theme(default_color=colorant"blue",        line_width=myLineWidth, default_point_size=myPointWidth) ),
            
-           Scale.x_log10(maxvalue=0.01), Guide.ylabel("CCSD Correlation Energy Recovered %"), Guide.xlabel("TCutPNO"),
-           Guide.manual_color_key("", ["DLPNO-CCSD (strong)", "DLPNO-CCSD (total)", "LPNO-CCSD (strong)", "LPNO-CCSD (total)"], ["orange", "red", "deepskyblue", "blue"]),
-           Coord.Cartesian(ymin=99, ymax=101, xflip=true), Stat.xticks(ticks=[-3, -4, -5, -6, -7, -8, -9, -10])
-           #Guide.yrug
+           Scale.x_log10(), Guide.ylabel("CCSD Correlation Energy Recovered %"), Guide.xlabel("TCutPNO"),
+           Guide.manual_color_key("", ["DLPNO-CCSD (strong)", "DLPNO-CCSD (total)", "LPNO-CCSD (strong)", "LPNO-CCSD (total)"], ["orange", "red", "deepskyblue", "blue"]),           
+           Coord.Cartesian(ymin=99, ymax=101, xflip=true),
+           Stat.xticks(ticks=[-3, -4, -5, -6, -7, -8, -9, -10]),
+           Stat.yticks(ticks=[99.0, 99.5, 100.0, 100.5, 101.0]),           
+           Guide.xticks(orientation=:horizontal),
+           Guide.yticks(ticks=[99.9])           
            #Stat.ygrid(ticks=[99.0, 99.5, 99.6, 99.7, 99.8, 99.9, 100.0, 100.5, 101.0]), 
   )
 
